e3355b9dc18e7984ebd0711733fae73abb0a5900
Hugo Coutinho <hugocoutinho96@outlook.com>
2024-02-18 19:56:29 -0300

    creating the sessions controller

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const routes = Router()
 const userRouter = require("./users.routes.js")
 const noteRouter = require("./notes.routes.js")
 const tagsRouter = require("./tags.routes.js")
+const sessionsRouter = require("./sessions.routes.js")
 
 routes.use("/users", userRouter)
 routes.use("/notes", noteRouter)
 routes.use("/tags", tagsRouter)
+routes.use("/sessions", sessionsRouter)
 
 module.exports = routes
