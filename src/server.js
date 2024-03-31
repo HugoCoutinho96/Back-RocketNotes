6713f05f929b522586fb4dc865e1d942a712f7a3
Hugo Coutinho <hugocoutinho96@outlook.com>
2024-03-30 22:39:50 -0300

    add pm2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require("express-async-errors")
+require("dotenv/config")
 const express = require("express")
 const app = express()
-const port = 3333
+const port = process.env.PORT || 3333
 
 const uploadsConfig = require("./configs/upload")
 
